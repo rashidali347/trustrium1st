3d8837b30c96263fd9e440da6b0e7231d92186cd
rashidali347 <rashidali47083490@gmail.com>
2026-05-11 03:21:51 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,7 +9,8 @@ const config: CapacitorConfig = {
     // It allows the app to handle routing and cookies like a real website.
     androidScheme: 'https',
     hostname: 'app.trustrium.com',
-    cleartext: true
+    cleartext: true,
+    allowNavigation: ['app.trustrium.com', '*.trustrium.com']
   }
 };
 
